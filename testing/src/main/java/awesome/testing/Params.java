3993900b678837ae2b1d80ea760f18fed3137aa9
Arik Hadas <arik.hadas@openu.ac.il>
2014-08-23 23:41:33 +0300

    remove unused import

--- testing/src/main/java/awesome/testing/Params.java
+++ testing/src/main/java/awesome/testing/Params.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import com.beust.jcommander.IStringConverter;
 import com.beust.jcommander.Parameter;
-import com.beust.jcommander.Parameters;
 
 //@Parameters(separators="=")
 public class Params {
